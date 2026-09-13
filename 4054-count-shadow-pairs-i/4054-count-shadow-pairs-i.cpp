008af3b4d6b0818ff39a2e7ba1874243ed1d23b0
NamanSharma72 <namaniit2004@gmail.com>
2026-09-13 13:58:41 +0530

    Time: 606 ms (9.14%), Space: 376.8 MB (10.04%) - LeetHub

--- 4054-count-shadow-pairs-i/4054-count-shadow-pairs-i.cpp
+++ 4054-count-shadow-pairs-i/4054-count-shadow-pairs-i.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     long long shadowPairs(vector<int>& nums) {
-        vector<int> st;
-        long long ans = 0;
-        for (int x : nums) {
-            // Count eligible previous values < x
-            int cnt = lower_bound(st.begin(), st.end(), x) - st.begin();
-            ans += cnt;
-            // x invalidates all previous values > x
-            while (!st.empty() && st.back() > x) st.pop_back();
-            // x itself becomes eligible for future elements
-            st.push_back(x);
+        vector<int> nextSmaller(nums.size());
+        stack<int> st;
+        unordered_map<int , vector<int>> mpp;
+        for(int i = 0 ; i<nums.size() ; i++) mpp[nums[i]].push_back(i);
+        long long count = 0;
+        for(int i = nums.size()-1 ; i>=0 ; i--){
+            while(!st.empty() && (nums[i] <= nums[st.top()])) st.pop();
+            if(st.empty()) nextSmaller[i] = nums.size();
+            else nextSmaller[i] = st.top();
+            st.push(i);
+            int ans = nextSmaller[i] - i - 1;
+            int left = upper_bound(mpp[nums[i]].begin() , mpp[nums[i]].end() , i) - mpp[nums[i]].begin();
+            int right = lower_bound(mpp[nums[i]].begin() , mpp[nums[i]].end() , nextSmaller[i]) - mpp[nums[i]].begin();
+            ans = ans - (right - left);
+            count+=ans;
         }
-        return ans;
+        return count;
     }
 };
