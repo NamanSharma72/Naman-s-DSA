979bf3c771b052d46fedd72e15a05e45c2411b56
NamanSharma72 <namaniit2004@gmail.com>
2026-09-16 19:50:28 +0530

    Time: 31 ms (66.36%), Space: 32.2 MB (43.46%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
-    int dp[1001][1001];
-    int doit(int start , int count , int n , int k){
-        if(count == k) return 1;
-        if(start == n-1) return 0;
-        if(dp[start][count] != -1) return dp[start][count];
-        int skip = doit(start+1 , count , n , k);
-        int take = 0;
-        for(int i = start+1 ; i<n ; i++){
-            take = (take + doit(i , count+1 , n , k))%mod;
-        }
-        return dp[start][count] = (skip + take)%mod;
-    }
     int numberOfSets(int n, int k) {
-        if(n == 1000 && k == 999) return 1;
-        memset(dp , -1 , sizeof(dp));
-        return doit(0 , 0 , n , k);
+        vector<vector<int>> dp(n , vector<int>(k+1 , 0));
+        for(int start = 0 ; start<n ; start++) dp[start][k] = 1;
+        for(int count = k-1 ; count>=0 ; count--){
+            int suffix = 0;
+            for(int start = n-2 ; start >=0 ; start--){
+                suffix = (suffix + dp[start+1][count+1])%mod;
+                int skip = dp[start+1][count];
+                int take = suffix;
+                dp[start][count] = (skip + take)%mod;
+            }
+        }
+        return dp[0][0];
     }
 };
