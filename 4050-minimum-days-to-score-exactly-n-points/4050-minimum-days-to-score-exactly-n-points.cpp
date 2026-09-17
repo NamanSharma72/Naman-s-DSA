e2747c76587bbe9c1be077e497da45aed98f7d95
NamanSharma72 <namaniit2004@gmail.com>
2026-09-17 19:14:39 +0530

    Time: 180 ms (93.21%), Space: 15.4 MB (72.01%) - LeetHub

--- 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int dp[100001];
-    int doit(int n){
-        if(n == 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int take = INT_MAX;
-        int days = 1;
-        while(true){
-            int points = (days *(days+1))/2;
-            if(points > n) break;
-            int ans;
-            if(n-points == 0) ans = days + doit(n - points);
-            else ans = 1 + days + doit(n - points);
-            take = min(take , ans);
-            days++;
-        }
-        return dp[n] = take;
-    }
     int minDays(int n) {
-        memset(dp , -1 , sizeof(dp));
-        return doit(n);
+        vector<int> dp(n+1 , INT_MAX);
+        dp[0] = 0;
+        for(int i = 1 ; i<= n ; i++){
+            int take = INT_MAX;
+            int days = 1;
+            while(true){
+                int points = (days * (days + 1))/2;
+                if(points > i) break;
+                if(i-points == 0) take = min(take , days);
+                else take = min(take , 1 + days + dp[i-points]);
+                days++;
+            }
+            dp[i] = take;
+        }
+        return dp[n];
     }
 };
