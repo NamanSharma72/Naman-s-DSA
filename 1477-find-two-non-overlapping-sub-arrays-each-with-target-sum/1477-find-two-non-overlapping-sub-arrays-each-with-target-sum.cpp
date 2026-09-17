08bd63c7271ced7e5cf5923fc0c51d3d2b9894a8
NamanSharma72 <namaniit2004@gmail.com>
2026-09-17 17:46:54 +0530

    Time: 183 ms (27.06%), Space: 142.5 MB (24.74%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    int dp[100000][3];
-    int doit(int idx , vector<pair<int,int>> &vt , int k){
-        if(k == 2) return 0;
-        if(idx == vt.size()) return 1e9;
-        if(dp[idx][k] != -1) return dp[idx][k];
-        int skip = doit(idx +1 , vt , k);
-        int low = idx+1 , high = vt.size()-1 , index = vt.size();
-        while(low <= high){
-            int mid = low + ((high-low)/2);
-            if(vt[mid].first > vt[idx].second){
-                index = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
-        }
-        int take = (vt[idx].second - vt[idx].first + 1) + doit(index , vt , k+1);
-        return dp[idx][k] = min(skip , take);
-    }
     int minSumOfLengths(vector<int>& arr, int target) {
         unordered_map<int , int> mpp;
-        vector<pair<int,int>> vt;
+        mpp[0] = -1;
+        int minAns = INT_MAX;
         int sum = 0;
-        mpp[sum] = -1;
+        vector<int> minTillI(arr.size() , 1e9);
         for(int i = 0 ; i<arr.size() ; i++){
             sum+=arr[i];
+            if(i > 0) minTillI[i] = minTillI[i-1];
             int left = sum - target;
-            if(mpp.find(left) != mpp.end()) vt.push_back({mpp[left]+1 , i});
+            if(mpp.find(left) != mpp.end()){
+                if(mpp[left] >= 0) minAns = min(minAns , i-mpp[left] + minTillI[mpp[left]]);
+                if(i > 0) minTillI[i] = min(minTillI[i-1] , i - mpp[left]);
+                else minTillI[i] = 1;
+            }
             mpp[sum] = i;
         }
-        if(vt.size() <= 1) return -1;
-        memset(dp , -1 , sizeof(dp));
-        int ans = doit(0 , vt , 0);
-        if(ans == 1e9) return -1;
-        return ans;
+        if(minAns >= 1e9) return -1;
+        return minAns;
     }
 };
