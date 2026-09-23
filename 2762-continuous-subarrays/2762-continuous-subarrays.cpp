3f7aa7211b305e11776e13e7eb410c34dd30293c
NamanSharma72 <namaniit2004@gmail.com>
2026-09-23 22:10:06 +0530

    Time: 97 ms (55.97%), Space: 121.7 MB (37.99%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.cpp
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -5,13 +5,9 @@ public:
         long long count = 0;
         int l = 0 , r = 0;
         while(r < nums.size()){
-            while(!maxi.empty() && nums[r] > maxi.back()){
-                maxi.pop_back();
-            }
+            while(!maxi.empty() && nums[r] > maxi.back()) maxi.pop_back();
             maxi.push_back(nums[r]);
-            while(!mini.empty() && nums[r] < mini.back()){
-                mini.pop_back();
-            }
+            while(!mini.empty() && nums[r] < mini.back()) mini.pop_back();
             mini.push_back(nums[r]);
             while(abs(maxi.front() - nums[r]) > 2 || abs(mini.front() - nums[r]) > 2){
                 if(!maxi.empty() && nums[l] == maxi.front()) maxi.pop_front();
