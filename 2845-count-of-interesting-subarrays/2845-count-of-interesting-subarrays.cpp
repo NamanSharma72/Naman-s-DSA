c26561eb5fa4aa65e92de98df6c3e39d39651f43
NamanSharma72 <namaniit2004@gmail.com>
2026-09-25 19:44:15 +0530

    Time: 1624 ms (5%), Space: 119.7 MB (67.69%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -3,11 +3,11 @@ public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         int count = 0;
         vector<int> modK(nums.size());
-        unordered_map<int , int> mpp;
+        // unordered_map<int , int> mpp;
         for(int i = 0 ; i<nums.size() ; i++){
             if(nums[i]%modulo == k) count++;
             modK[i] = count;
-            if(mpp.find(count) == mpp.end()) mpp[count] = i;
+            // if(mpp.find(count) == mpp.end()) mpp[count] = i;
         }
         for(int i = 0 ; i<nums.size() ; i++) cout << modK[i] << " ";
         long long finalAns = 0;
@@ -15,10 +15,8 @@ public:
         int count1 = 0;
         for(int i = 0 ; i<nums.size() ; i++){
             if(modK[i]%modulo == k) finalAns++;
-            // else{
-                int left = (modK[i]-k)%modulo;
-                if(mpp1.find(left) != mpp1.end()) finalAns+=mpp1[left];
-            // }
+            int left = (modK[i]-k)%modulo;
+            if(mpp1.find(left) != mpp1.end()) finalAns+=mpp1[left];
             if(nums[i]%modulo == k) count1++;
             mpp1[count1%modulo]++;
         }
